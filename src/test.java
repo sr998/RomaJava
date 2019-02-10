eed3a96e4c530936bae26b71bedd9e4999a57d2c
sr998 <37926213+sr998@users.noreply.github.com>
2019-02-10 18:57:04 +0200

    src folder added

---
+++ src/test.java
@@ -0,0 +1,50 @@
+import java.util.Scanner; // імпорт сканера
+
+
+public class test {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Перший урок.");
+        System.out.println("Виберіть одне з наступних завдань:");
+        System.out.println("1 - обчислити площу кола,по заданому радіусу");
+        System.out.println("2 - обчислити гіпотенузу прямокутного трикутника з заданими катетами");
+        System.out.println("3 - робота з текстом");
+        System.out.println("4 - калькулятор для + і -");
+        int x = in.nextInt();
+        double k1, k2, gipotenuza;
+        int r, a, b;
+        double pi = 3.14;
+        switch (x) {
+             case 1:
+                System.out.println("Введіть значення радіусу кола: ");
+                r = in.nextInt();
+                System.out.println("Площа кола рівна: "+pi*r*r);
+                break;
+            case 2:
+                System.out.println("Введіть значення катету №1: ");
+                k1 = in.nextDouble();
+                System.out.println("Введіть значення катету №2: ");
+                k2 = in.nextDouble();
+                gipotenuza = k1 * k1 + k2 * k2;
+                System.out.println("Гіпотенуза рівна: " + Math.sqrt(gipotenuza));
+                break;
+            case 3:
+                System.out.println("\nМій ріядок №1");
+                System.out.println("\tМій ріядок №2");
+                System.out.println("aМій ріядок №3");
+                break;
+            case 4:
+                System.out.println("Введіть значення цілого числа №1: ");
+                a = in.nextInt();
+                System.out.println("Введіть значення цілого числа №2: ");
+                b = in.nextInt();
+                System.out.println("Сума цих двох чисел буде рівна: "+(a+b));
+                System.out.println("Різниця цих двох чисел буде рівна: "+(a-b));
+                break;
+            default:
+                System.out.println("Ви ввели некоректні дані. програму буде завершено");
+                break;
+
+        }
+    }
+}
